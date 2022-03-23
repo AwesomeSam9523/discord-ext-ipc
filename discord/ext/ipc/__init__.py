00b6a23eb3d67587b1f8a16e5afa069f1d547e21
Samaksh Gupta <samakshgupta04@gmail.com>
2022-03-23 10:25:46 +0530

    feat: Add slient.py

--- discord/ext/ipc/__init__.py
+++ discord/ext/ipc/__init__.py
@@ -1,11 +1,13 @@
 import collections
+import imp
 
 from discord.ext.ipc.client import Client
 from discord.ext.ipc.server import Server
+from discord.ext.ipc.slient import Slient
 from discord.ext.ipc.errors import *
 
 
 _VersionInfo = collections.namedtuple("_VersionInfo", "major minor micro release serial")
 
-version = "2.1.1"
-version_info = _VersionInfo(2, 1, 1, "final", 0)
+version = "2.1.2"
+version_info = _VersionInfo(2, 1, 2, "final", 0)
---
+++ discord/ext/ipc/slient.py
@@ -0,0 +1,42 @@
+'''
+Credits: BlackThunder#4007
+Source: https://github.com/BlackThunder01001/discord-ext-ipc/blob/main/discord/ext/ipc/slient.py
+'''
+
+from .client import Client
+from .server import Server
+
+class Slient(Client, Server):
+    """
+    Merges the methods of class:`.Client` & class:`.Server` to allow intercommunication
+    between two discord bots. 
+    """
+    def __init__(
+        self,
+        bot,
+        host="localhost",
+        base_port = 8654,
+        secret_key = None,
+        base_multicast = 20000,
+        instance = 0
+    ):
+        self.client = Client(
+            port = base_port + instance,
+            multicast_port = base_multicast +1-instance,
+            host = host,
+            secret_key = secret_key)
+
+        self.server = Server(
+            bot = bot,
+            port = base_port+1-instance,
+            multicast_port = base_multicast + instance,
+            host = host,
+            secret_key = secret_key
+        )
+        for com in (self.server, self.client):
+            for method in dir(com):
+                if not method.startswith('__'):
+                    try:
+                        setattr(self, method, getattr(com, method))
+                    except:
+                        pass
